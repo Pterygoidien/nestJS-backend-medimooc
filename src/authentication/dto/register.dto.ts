fe97c256f9203102021e32cefddde858c0293ee8
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2023-04-21 07:33:23 +0200

    adding class validation to REgisteroDTO

--- src/authentication/dto/register.dto.ts
+++ src/authentication/dto/register.dto.ts
@@ -1,5 +1,11 @@
+import { IsEmail, IsString, IsNotEmpty, MinLength } from "class-validator";
+
 export class RegisterDto {
+    @IsEmail()
+    @IsNotEmpty()
     email: string;
+    @IsString()
+    @IsNotEmpty()
     password: string;
 }
 export default RegisterDto;
